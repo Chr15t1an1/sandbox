2e1e99511118caa20fbb5a9a63d5cb13157841d8
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-23 05:23:32 -0500

    .

--- index.php
+++ index.php
@@ -5,34 +5,34 @@ ini_set('display_errors', '1');
 
 $x = http_build_query($_REQUEST);
 
-echo $x;
-//
-//
-//
-//     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-// try {
-//
-//       $field_em = $_POST['email_home'];
-//       $field_e = $_POST['event'];
-//       $fields = array($field_em,$field_e);
-//       $fp = fopen('file.csv', 'a');
-//       fputcsv($fp, $fields);
-//       echo "sucess";
-//
-//     } catch (Exception $e) {
-//         echo 'Caught exception: ',  $e->getMessage(), "\n";
-//
-//           $fp = fopen('error_requests.txt', 'a');
-//           $data = "\n".$e->getMessage();
-//           fwrite($handle, $data);
-//
-//           $data = "\n".$e->getMessage();
-//           fwrite($handle, $data);
-//
-//           fclose($handle);
-//
-//     }
-//
-//    }else {
-//      echo "Not a resource";
-//    }
+
+
+
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+try {
+
+      $field_em = $_POST['email_home'];
+      $field_e = $_POST['event'];
+      $fields = array($field_em,$field_e);
+      $fp = fopen('file.csv', 'a');
+      fputcsv($fp, $fields);
+      echo "sucess";
+
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+          $fp = fopen('error_requests.txt', 'a');
+          $data = "\n".$e->getMessage();
+          fwrite($handle, $data);
+
+          $data = $x;
+          fwrite($handle, $data);
+
+          fclose($handle);
+
+    }
+
+   }else {
+     echo "Not a resource";
+   }
