773781180fadb01f68ca46c4038ba21e5e1e3a18
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-23 04:32:57 -0500

    .
    .

--- index.php
+++ index.php
@@ -5,25 +5,13 @@
 
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-
-
       $field_em = $_POST['email'];
       $field_e = $_POST['event'];
-
-$a = $field_em.' '.$field_e;
-
-      echo $a;
-
-
-        // The request is using the POST method
-      //
-      // $fp = fopen('file.csv', 'w');
-      //
-      // fputcsv($fp, $fields);
-      //
-      //
-      //
-      // echo "sucess";
+      $fields = array($field_em,$field_e);
+      $fp = fopen('file.csv', 'w');
+      fputcsv($fp, $fields);
+      
+      echo "sucess";
 
 
 
