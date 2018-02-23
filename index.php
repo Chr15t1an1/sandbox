97c76fa8ff1ed6e73cd421cc3366081e64e4956b
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-22 22:59:23 -0500

    .test
    .

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Origin: https://www.knowledgecoop.com/');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 
 
 
