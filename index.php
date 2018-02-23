c40c86756001d3a649a620a14c50487b2fc1ba1f
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-23 04:24:01 -0500

    .
    .

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
 
-echo $_SERVER['REQUEST_METHOD'];
 
 
-//
-//     if ($_POST) {
-//
-//
-//       $field_em = $_POST['email'];
-//       $field_e = $_POST['event'];
-//
-// $a = $field_em.' '.$field_e;
-//
-//       echo $a;
-//
-//
-//         // The request is using the POST method
-//       //
-//       // $fp = fopen('file.csv', 'w');
-//       //
-//       // fputcsv($fp, $fields);
-//       //
-//       //
-//       //
-//       // echo "sucess";
-//
-//
-//
-//    }else {
-//      echo "Not a resource";
-//    }
-//
-// echo "last";
+
+
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+
+      $field_em = $_POST['email'];
+      $field_e = $_POST['event'];
+
+$a = $field_em.' '.$field_e;
+
+      echo $a;
+
+
+        // The request is using the POST method
+      //
+      // $fp = fopen('file.csv', 'w');
+      //
+      // fputcsv($fp, $fields);
+      //
+      //
+      //
+      // echo "sucess";
+
+
+
+   }else {
+     echo "Not a resource";
+   }
+
+echo "last";
