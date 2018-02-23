bdbcc56c301100ce5f7ce2b5fbb6aae228edb251
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-23 04:35:23 -0500

    .
    .

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
 
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
 
 
 
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+
+try {
+
       $field_em = $_POST['email'];
       $field_e = $_POST['event'];
       $fields = array($field_em,$field_e);
       $fp = fopen('file.csv', 'w');
       fputcsv($fp, $fields);
-      
       echo "sucess";
 
-
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
 
    }else {
      echo "Not a resource";
